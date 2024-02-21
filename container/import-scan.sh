d8369914f07884ba64125b9817f9fa474db2667d
David Anderson <david.n.anderson@gsa.gov>
2024-02-21 16:06:19 -0500

    add script to import scan to defect dojo

---
+++ container/import-scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Send POST request containing grype results to Defect Dojo
+
+curl -X 'POST' $DEFECTDOJO_IMPORT_URL \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -H 'Authorization: Token $DEFECTDOJO_AUTH_TOKEN' \
+  -F 'product_type_name=Container Image' \
+  -F 'active=true' \
+  -F 'minimum_severity=Info' \
+  -F 'verified=true' \
+  -F 'scan_type=Anchore Grype' \
+  -F 'product_name=${IMAGENAME}' \
+  -F 'engagement_name=$SCAN_TYPE' \
+  -F 'auto_create_context=true' \
+  -F 'file=@cves/output.json;type=application/json'

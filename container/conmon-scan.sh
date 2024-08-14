690a550c40cc3da5cb69add24d7d66a7d13add8e
David Anderson <david.n.anderson@gsa.gov>
2024-08-14 15:11:41 -0400

    fix typo

--- container/conmon-scan.sh
+++ container/conmon-scan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Generates XML formatted report
-grype image/image.tar -c common-pipelines/container/grype.yaml --only-fixed -q -o cyclonedx --file conmon-can/$IMAGENAME.xml
+grype image/image.tar -c common-pipelines/container/grype.yaml --only-fixed -q -o cyclonedx --file conmon-scan/$IMAGENAME.xml

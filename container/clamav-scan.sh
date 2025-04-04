70714b9cd8c609a8cf2db06a58ad5edb44f4fcc9
David Anderson <david.n.anderson@gsa.gov>
2025-04-04 16:41:51 -0400

    install clamav-daemon for config files and exclude directory from scan

--- container/clamav-scan.sh
+++ container/clamav-scan.sh
@@ -3,13 +3,13 @@
 echo "Updating packages"
 apt-get -qq update
 apt-get -y -qq install \
-  clamav
+  clamav clamav-daemon
 
 # Update database and run clamav scan
 echo "Running database check"
 freshclam
 
 echo "Running clamav scan"
-clamscan -r -i /
+clamscan --exclude /sys -r -i /
 
 echo "Clamav scan is done"
